4eb3eab7e237a244aa5c980a0808ed4f8b6eb5ba
Vivek <vivek131299@gmail.com>
2024-08-03 19:20:59 +0530

    Added chat request send and accept functionality

--- chatify/chat/urls.py
+++ chatify/chat/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('send_request/<int:pk>/', views.send_request, name='send_request'),
+    path('requests/', views.view_requests, name='requests'),
+    path('accept_request/<int:sent_from_id>/<int:sent_to_id>/', views.accept_request, name='accept_request'),
+    path('accept_request_by_id/<int:id>/', views.accept_request_by_id, name='accept_request_by_id'),
 ]
--- chatify/chat/views.py
+++ chatify/chat/views.py
@@ -16,14 +16,23 @@ def home(request):
     if request.method == 'GET':
         users = list(User.objects.exclude(username=request.user.username))
         sent_requests = list(Request.objects.filter(request_sent_from_id=request.user.id))
-        sent_requests_to_users = [request.request_sent_to for request in sent_requests]
-        accepted_requests_users = [request.request_sent_to for request in sent_requests if request.accepted]
+        received_requests = list(Request.objects.filter(request_sent_to_id=request.user.id))
+        sent_requests_to_users = [r.request_sent_to for r in sent_requests]
+        accepted_sent_requests_to_users = [r.request_sent_to for r in sent_requests if r.accepted]
+        received_requests_from_users = [r.request_sent_from for r in received_requests]
+        accepted_received_requests_from_users = [r.request_sent_from for r in received_requests if r.accepted]
         print(sent_requests_to_users)
-        return render(request, "home.html", {'users': users, 'sent_requests_to_users': sent_requests_to_users, 'accepted_requests_users': accepted_requests_users})
+        return render(request, "home.html",
+                      {'users': users,
+                       'sent_requests_to_users': sent_requests_to_users,
+                       'accepted_sent_requests_to_users': accepted_sent_requests_to_users,
+                       'received_requests_from_users': received_requests_from_users,
+                       'accepted_received_requests_from_users': accepted_received_requests_from_users})
 
 
 @csrf_exempt
 @login_required
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def send_request(request, pk=None):
     if request.method == 'GET':
         if pk and pk != request.user.id:
@@ -35,6 +44,47 @@ def send_request(request, pk=None):
                 return redirect("home")
         return redirect("home")
 
+
+@csrf_exempt
+@login_required
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def view_requests(request):
+    if request.method == 'GET':
+        requests = Request.objects.all()
+        print(requests)
+        sent_requests = request.user.sent_requests.all()
+        not_accepted_sent_requests = [r for r in sent_requests if not r.accepted]
+        received_requests = request.user.received_requests.all()
+        not_accepted_received_requests = [r for r in received_requests if not r.accepted]
+        print(sent_requests)
+        print(received_requests)
+        return render(request, "requests.html", {'not_accepted_sent_requests': not_accepted_sent_requests, 'not_accepted_received_requests': not_accepted_received_requests})
+
+
+@csrf_exempt
+@login_required
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def accept_request(request, sent_from_id=None, sent_to_id=None):
+    if request.method == 'GET':
+        if sent_from_id and sent_to_id:
+            chat_request = Request.objects.get(request_sent_from_id=sent_from_id, request_sent_to_id=sent_to_id)
+            chat_request.accepted = True
+            chat_request.save()
+
+            return redirect("home")
+
+
+@csrf_exempt
+@login_required
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def accept_request_by_id(request, id=None):
+    if request.method == 'GET':
+        if id:
+            chat_request = Request.objects.get(id=id)
+            chat_request.accepted = True
+            chat_request.save()
+
+            return redirect("home")
 
 
 def register(request):
